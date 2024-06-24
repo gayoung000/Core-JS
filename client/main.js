1762fa956def995e81b2124a864636498a01dadb
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-06-24 17:47:30 +0900

    learn. jujeob generator

--- client/main.js
+++ client/main.js
@@ -1,5 +1,44 @@
+import data from "./data/data.js";
+import { 
+  getNode, 
+  getRandom, 
+  insertLast, 
+  clearContents 
+} from './lib/index.js'
 
 
+
+// [phase-1]
+// 1. 주접 떨기 버튼을 클릭 하는 함수
+//    - 주접 떨기 버튼 가져오기
+//    - 이벤트 연결하기 addEventListener('click')
+
+// 2. input 값 가져오기
+//    - input.value
+
+// 3. data함수에서 주접 1개 꺼내기
+//    - data(name)
+//    - getRandom()
+
+// 4. pick 항목 랜더링하기
+
+
+
+const submit = getNode('#submit');
+const nameField = getNode('#nameField');
+const result = getNode('.result');
+
+function handleSubmit(e){
+  e.preventDefault();
+
+  const name = nameField.value;
+  const list = data(name);
+  const pick = list[getRandom(list.length)];
+  let output = result.textContent = pick;
+  console.log(output);
+}
+
+submit.addEventListener('click', handleSubmit)
 
 
 
