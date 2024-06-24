fce1968bc5ca5ccc83dec51b44065cb2ef484d48
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-06-24 14:45:59 +0900

    범쌤 #03.calculator client/lib 파일 degit하기, 나머지 함수 export-import 하기

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
-const server = require("live-server");
 
-const params = {
-  host: "localhost",
-  port: 3000,
-  open: false,
-  root: "./client",
-};
 
-server.start(params);
+
+// const server = require('live-server');
+
+// const params = {
+//   host: 'localhost',
+//   port: 3000,
+//   open: false,
+//   root: './client'
+// }
+
+
+// server.start(params)
+
+
+console.log(global)
+
+
+
+
+
+
+
+
+
+
